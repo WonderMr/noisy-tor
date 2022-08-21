dea061fa77201858c4a43006fcf3ad7d1144b411
154pinkchairs <ovehis@riseup.net>
2022-08-22 01:15:46 +0200

    fix some syntax errors

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
-import requests, lxml
+import requests
+import lxml
 from bs4 import BeautifulSoup as bs
 headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
@@ -9,18 +10,18 @@ safari = requests.get("https://www.whatismybrowser.com/guides/the-latest-user-ag
 edge = requests.get("https://www.whatismybrowser.com/guides/the-latest-user-agent/edge", headers=headers)
 opera = requests.get("https://www.whatismybrowser.com/guides/the-latest-user-agent/opera", headers=headers)
 
-soup = BeautifulSoup(html.text, "lxml")
+soup = bs(html.text, "lxml")
 
-def uasw:{
+def uasw():
     for ua in soup.select(".code"):
         with open('config.json', 'r+') as c:
             for line in c:
                 if line.contains('user_agents'):
-                    lines[i] = lines[i].strip() + c.write(f""{ua}",\n"{ua}"")
+                    lines[i] = lines[i].strip() + c.write("\"{ua}\",\n\"{ua}\"")
             c.seek(0)
             for line in lines:
                 c.write(line)
-    }
+    
 uasw.chrome()
 uasw.ff()
 uasw.safari()
