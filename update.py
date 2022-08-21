a65721467137b6616c849a74034abef18c9ceb1f
154pinkchairs <ovehis@riseup.net>
2022-08-22 01:00:10 +0200

    fix typo

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-impost requests, lxml
+import requests, lxml
 from bs4 import BeautifulSoup as bs
 headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
